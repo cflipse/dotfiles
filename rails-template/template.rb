7c6657ea9638821024d76b5324f48ddffdafb2ce
Chris Flipse <cflipse@gmail.com>
2021-11-09 19:16:18 -0500

    start tinkering with rubocop

--- rails-template/template.rb
+++ rails-template/template.rb
@@ -2,43 +2,47 @@ def source_paths
   [__dir__]
 end
 
-copy_file 'direnv', '.envrc'
-run 'direnv allow'
-run 'bundle  config --local path .bundle/gems'
+copy_file "direnv", ".envrc"
+run "direnv allow"
+run "bundle  config --local path .bundle/gems"
 
-uncomment_lines 'Gemfile', /gem ['"]redis['"]/
+uncomment_lines "Gemfile", /gem [""]redis[""]/
 
 gem_group :test do
-  gem 'rspec-rails'
-  gem 'capybara'
+  gem "rspec-rails"
+  gem "capybara"
 end
 
 gem_group :development, :test do
-  gem 'dotenv-rails'
-  gem 'brakeman'
+  gem "dotenv-rails"
+  gem "brakeman"
 end
 
-gem 'rubocop', require: false
-gem 'rubocop-rspec', require: false
-gem 'rubocop-rails', require: false
+gem "rubocop", require: false
+gem "rubocop-rspec", require: false
+gem "rubocop-rails", require: false
 
-gem 'view_components'
+gem "view_components"
+
+gem "vite_rails"
 
 # copy_file "dockerignore", ".dockerignore"
 # copy_file "Dockerfile"
 
-copy_file 'rubocop.yml', '.rubocop.yml'
-copy_file 'rubocop_pending.yml', '.rubocop_pending.yml'
-copy_file 'rubocop_todo.yml', '.rubocop_todo.yml'
+copy_file "rubocop.yml", ".rubocop.yml"
 
 after_bundle do
-  generate 'rspec:install'
+  run "bundle exec vite install"
+end
 
-  run 'rubocop -A'
+after_bundle do
+  generate "rspec:install"
+
+  run "rubocop -A"
 end
 
 after_bundle do
   git :init
-  git add: '.'
+  git add: "."
   git commit: "-m 'initial commit'"
 end
